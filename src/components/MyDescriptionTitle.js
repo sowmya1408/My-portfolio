d37f854b36801278b45048ab69adab2abc16e4a7
Sowmya Mannem <sowmyamannemr@gmail.com>
2020-08-20 14:59:30 +0200

    deleted non-related files and added components folder

---
+++ src/components/MyDescriptionTitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyDescriptionTitle = () => {
+    return (
+        <div>
+            <h1>front end focused with full stack experience</h1>
+        </div>
+    )
+}
+
+export default MyDescriptionTitle;
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <div className="title-container">
+            <h1>sowmya mannem</h1>
+            <h3>front end engineer</h3>     
+        </div>
+    )
+}
+
+export default Title
