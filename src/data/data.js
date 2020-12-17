45a38a524c0953c858758ab870a709b959bd94df
sowmya1408 <sowmyamannemr@gmail.com>
2020-12-17 19:25:40 +0100

    added all the project to data to show in projects section

--- src/data/data.js
+++ src/data/data.js
@@ -1,29 +1,145 @@
-export const cardData = [
-	{
-		id: 1,
-		img:
-			'https://static.onecms.io/wp-content/uploads/sites/24/2018/01/soba_noodle-and-shrimp_bowl_2554701_qfsfe_01_1-2000.jpg',
-		name: 'Meal-Sharing App',
-		technologies: ['Javascript', 'CSS', 'Webpack', 'Heroku'],
-		Sourcecode: 'https://github.com/sowmya1408/Fullstack--app-Meal-sharing-Website-',
-		Deployed: 'https://meal-sharingapp.herokuapp.com/',
-	},
-	{
-		id: 2,
-		img: 'https://justfreewpthemes.com/wp-content/uploads/2019/06/WordPress-Quiz-plugin.jpg',
-		name: 'Quiz App',
-		technologies: ['React', 'CSS', 'Open source API', 'Typescript'],
-		Sourcecode: 'https://github.com/sowmya1408/React-QuizApp',
-		Deployed: 'https://react-typescript-quizz-app.herokuapp.com/',
-	},
-	{
-		id: 3,
-		img:
-			'https://cdn.vox-cdn.com/thumbor/KfXN2zffBnk8J7QbRfaFNSkOFbI=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19552207/pokemon_tower_battle.jpg',
-		name: 'Pokemon App',
-		technologies: ['React', 'CSS', 'Open source API', 'Heroku'],
-		Sourcecode: 'https://github.com/sowmya1408/PokemonApp-task',
-		Deployed: 'https://pokemons-search-app.herokuapp.com/',
-	},
-];
+import banking from "../images/banking.png";
+import covidApp from "../images/covid-app.png";
+import cycling from "../images/cycling-insights.png";
+import dashboard from "../images/dashboard.png";
+import eCommerce from "../images/e-commerce.png";
+import hotelBooking from "../images/hotel-booking.png";
+import memeGenerator from "../images/meme-generator.png";
 
+export const cardData = [
+  {
+    id: 1,
+    img:
+      "https://static.onecms.io/wp-content/uploads/sites/24/2018/01/soba_noodle-and-shrimp_bowl_2554701_qfsfe_01_1-2000.jpg",
+    name: "Meal-Sharing App",
+    technologies: ["Javascript", "CSS", "Webpack", "Heroku"],
+    Sourcecode:
+      "https://github.com/sowmya1408/Fullstack--app-Meal-sharing-Website-",
+    Deployed: "https://meal-sharingapp.herokuapp.com/",
+  },
+  {
+    id: 2,
+    img:
+      "https://justfreewpthemes.com/wp-content/uploads/2019/06/WordPress-Quiz-plugin.jpg",
+    name: "Quiz App",
+    technologies: ["React", "CSS", "Open source API", "Typescript"],
+    Sourcecode: "https://github.com/sowmya1408/React-QuizApp",
+    Deployed: "https://react-typescript-quizz-app.herokuapp.com/",
+  },
+  {
+    id: 3,
+    img:
+      "https://cdn.vox-cdn.com/thumbor/KfXN2zffBnk8J7QbRfaFNSkOFbI=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19552207/pokemon_tower_battle.jpg",
+    name: "Pokemon App",
+    technologies: ["React", "CSS", "Open source API", "Heroku"],
+    Sourcecode: "https://github.com/sowmya1408/PokemonApp-task",
+    Deployed: "https://pokemons-search-app.herokuapp.com/",
+  },
+  {
+    id: 4,
+    img: covidApp,
+    name: "Covid App",
+    technologies: ["React", "CSS", "Open source API", "Mapbox", "Heroku"],
+    Sourcecode: "https://github.com/sowmya1408/covidapp_Mapbox",
+    Deployed: "https://covidapp-data.herokuapp.com/",
+  },
+  {
+    id: 5,
+    img: cycling,
+    name: "Cycling-insights App",
+    technologies: [
+      "React",
+      "CSS",
+      "Open source API",
+      "Mapbox",
+      "Re-charts",
+      "Heroku",
+    ],
+    Sourcecode: "https://github.com/sowmya1408/Copenhagen-cycling-insights",
+    Deployed: "https://cph-cycling-insights.herokuapp.com/",
+  },
+  {
+    id: 6,
+    img: eCommerce,
+    name: "E-commerce App",
+    technologies: [
+      "React",
+      "Redux",
+      "Javascript",
+      "CSS",
+      "MySQL",
+      "Node.js",
+      "Express.js",
+      "Heroku",
+    ],
+    Sourcecode: "https://github.com/sowmya1408/E-commerce-Fullstack",
+    Deployed: "https://clothingapp-frontend.herokuapp.com/",
+  },
+  {
+    id: 7,
+    img: hotelBooking,
+    name: "Hotel-booking App",
+    technologies: [
+      "React",
+      "Javascript",
+      "CSS",
+      "Mongo db",
+      "Node.js",
+      "Express.js",
+      "Heroku",
+    ],
+    Sourcecode: "https://github.com/sowmya1408/Frontend-hotel-booking-app",
+    Deployed: "https://hotelbooking-application.herokuapp.com/",
+  },
+  {
+    id: 8,
+    img: memeGenerator,
+    name: "Meme-generator App",
+    technologies: [
+      "React",
+      "Javascript",
+      "CSS",
+      "Open source API",
+      "Codesandbox",
+    ],
+    Sourcecode: "https://github.com/sowmya1408/React-meme-Project",
+    Deployed: "https://codesandbox.io/s/github/sowmya1408/React-meme-Project",
+  },
+  {
+    id: 9,
+    img: dashboard,
+    name: "Dashboard App",
+    technologies: [
+      "React",
+      "Javascript",
+      "storybook",
+      "Re-charts",
+      "Firebase",
+      "MySQL",
+      "Knex.js",
+      "Node.js",
+      "Express.js",
+      "Heroku",
+    ],
+    Sourcecode: "https://github.com/sowmya1408/FP-class11",
+    Deployed: "https://staging-fp-class11.herokuapp.com/",
+  },
+  {
+    id: 10,
+    img: banking,
+    name: "Banking App",
+    technologies: [
+      "React",
+      "Javascript",
+      "Firebase",
+      "Mongo db",
+      "Node.js",
+      "Express.js",
+      "Load balancer",
+      "SSL(https)",
+      "Heroku",
+    ],
+    Sourcecode: "https://github.com/sowmya1408/Banking-app-frontend",
+    Deployed: banking,
+  },
+];
